283a4a3e051248913048404f97180f3a39fe171a
AidenWTF <hi@aiden.wtf>
2021-11-14 18:32:44 -0800

    idk

--- adalaat-site/src/App.js
+++ adalaat-site/src/App.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {Suspense, useRef } from "react";
 import "./App.scss";
 //Components
 import Header from "./components/header";
-import { Canvas } from "react-three-fiber"
-import {Html} from 'drei'
+import { Canvas, useFrame } from "react-three-fiber"
+import {Html, useGLTFLoader} from 'drei'
+import { Section } from "./components/section";
+
+const Model = ({modelPath}) => {
+  const gltf = useGLTFLoader(modelPath, true)
+  return <primitive object={gltf.scene} dispose={null} />
+}
+
+const Lights = ( ) => {
+  return (
+    <>
+    <ambientLight intensity={0.3} />
+    <directionalLight position={[10, 10, 5]} intensity={1} />
+    <directionalLight position={[0, 10, 0]} intensity={1.5} />
+    <spotLight position={[1000, 0, 0]} intensity={1} />
+    </>
+  )
+}
+
+const HTMLContent = ({children, modelPath, positionY}) => {
+
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y += 0.005));
+
+  return (
+  <Section factor={1.5} offset={1}>
+    <group position={[0, positionY, 0]}>
+      <mesh ref={ref} position={[0, -100, -800]}>
+        <Model modelPath={modelPath} />
+      </mesh>
+      <Html fullscreen>
+          {children}
+      </Html>
+    </group>
+  </Section>  
+ );
+};
+
 
 export default function App() {
   return (
@@ -12,11 +49,21 @@ export default function App() {
       <Canvas
         colorManagement
         camera={{position:[0,0,120], fov: 70}}>
-          <Html fullscreen>
-            <div className='container'>
-              <h1 className='title'>Hello</h1>            
-            </div>
-          </Html>
+          <Lights />
+          <Suspense fallback={null}>
+         {/* <HTMLContent modelPath='/scene.gltf' positionY={250}> 
+                
+                <div className='container'>
+                  <h1 className='title'>Hello</h1>            
+                </div>
+          </HTMLContent > */}
+          <HTMLContent modelPath='/scene.gltf' positionY={250}> 
+                
+                <div className='container'>
+                  <h1 className='title'>Hello</h1>            
+                </div>
+          </HTMLContent >
+          </Suspense>
       </Canvas>
     </>
   );
